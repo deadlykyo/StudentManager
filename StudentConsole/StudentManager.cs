a8a7860bbb232ed04b91baf5662096fe29784a9b
Edson Gonzales <edson.gonzales@meta-dao.com>
2020-02-09 18:50:58 -0400

    Added some try catchs on the main method

--- StudentConsole/StudentManager.cs
+++ StudentConsole/StudentManager.cs
@@ -1,5 +1,6 @@
 ﻿using StudentModel.Entities;
 using StudentProcessor;
+using StudentProcessor.Exceptions;
 using StudentProcessor.Extensions;
 using StudentProcessor.Readers;
 using StudentProcessor.Tools;
@@ -17,66 +18,92 @@ namespace ConsoleStudent
 
         internal List<StudentEntity> LoadRecordsFromFile(string filePath)
         {
-            List<StudentEntity> result = new List<StudentEntity>();
-
-            if (File.Exists(filePath))
+            try
             {
-                FileReader reader = new FileReader();
-                reader.SetReader(new CsvReader());
-                reader.SetFileName(filePath);
-                result = reader.ReadRecords();
+                List<StudentEntity> result = new List<StudentEntity>();
+
+                if (File.Exists(filePath))
+                {
+                    FileReader reader = new FileReader();
+                    reader.SetReader(new CsvReader());
+                    reader.SetFileName(filePath);
+                    result = reader.ReadRecords();
+                }
+                return result;
             }
-            return result;
+            catch (Exception ex)
+            {
+                string errorMessage = $"Error loading records from CSV '{filePath}'";
+                throw new StudentException(errorMessage, ex);
+            }
         }
 
         internal void DisplayRecords(List<StudentEntity> results)
         {
-            //Filtering data before display it
-            if (_filters != null && _filters.Count > 0)
-                results = results.Where(_filters).ToList();
-
-            //Sorting data before display it
-            if (_sortByColumns != null && _sortByColumns.Count > 0)
-                results = results.SortBy(_sortByColumns).ToList();
-
-            foreach (var entity in results)
+            try
             {
-                Console.WriteLine(entity.ToString());
+                //Filtering data before display it
+                if (_filters != null && _filters.Count > 0)
+                    results = results.Where(_filters).ToList();
+
+                //Sorting data before display it
+                if (_sortByColumns != null && _sortByColumns.Count > 0)
+                    results = results.SortBy(_sortByColumns).ToList();
+
+                foreach (var entity in results)
+                {
+                    Console.WriteLine(entity.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = $"Error displaying records from the list of students.";
+                throw new StudentException(errorMessage, ex);
             }
         }
 
         internal void SetFilters(List<string> csvFilters)
         {
-            KeyValuePair<string, object> filterDefinition = new KeyValuePair<string, object>();
-            _sortByColumns = new List<string>();
-
-            foreach (var filter in csvFilters)
+            try
             {
-                var filterProps = filter.Split('=');
+                KeyValuePair<string, object> filterDefinition = new KeyValuePair<string, object>();
+                _sortByColumns = new List<string>();
 
-                switch (filterProps[0].ToLower())
+                foreach (var filter in csvFilters)
                 {
-                    case "gender":
-                        _sortByColumns.Add("LastUpdated|1");
-                        filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
-                        break;
-                    case "stage":
-                        _sortByColumns.Add("LastUpdated|1");
-                        filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
-                        break;
-                    case "lastupdated":
-                        _sortByColumns.Add("LastUpdated|0");
-                        filterDefinition = new KeyValuePair<string, object>(filterProps[0], FieldParser.ConvertToDate(filterProps[1]));
-                        break;
-                    default:
-                        _sortByColumns.Add("Name");
-                        filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
-                        break;
+                    var filterProps = filter.Split('=');
+
+                    switch (filterProps[0].ToLower())
+                    {
+                        case "gender":
+                            _sortByColumns.Add("LastUpdated|1");
+                            filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
+                            break;
+                        case "stage":
+                            _sortByColumns.Add("LastUpdated|1");
+                            filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
+                            break;
+                        case "lastupdated":
+                            _sortByColumns.Add("LastUpdated|0");
+                            filterDefinition = new KeyValuePair<string, object>(filterProps[0], FieldParser.ConvertToDate(filterProps[1]));
+                            break;
+                        default:
+                            _sortByColumns.Add("Name");
+                            filterDefinition = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
+                            break;
+                    }
+
+                    var filterProp = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
+
+                    _filters.Add(filterProp);
                 }
 
-                var filterProp = new KeyValuePair<string, object>(filterProps[0], filterProps[1]);
-
-                _filters.Add(filterProp);
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = $"Error setting filters for the list of students.";
+                throw new StudentException(errorMessage, ex);
             }
         }
     }
